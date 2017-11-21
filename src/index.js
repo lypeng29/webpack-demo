b41b55d60f067f57b6106273c3fcde58bb0b1011
lypeng <893371810@qq.com>
2017-11-21 14:14:05 +0800

    htmlwebpackplugin is over

--- src/index.js
+++ src/index.js
@@ -1,18 +1,13 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './search.png';
+import printMe from './print.js';
 
 function component() {
   var element = document.createElement('div');
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('red');
-
-  var myIcon = new Image();
-  myIcon.src = Icon;
-
-  element.appendChild(myIcon);
-
-
+  var btn = document.createElement('button');
+  btn.innerHTML = 'click me and check the console!';
+  btn.onclick = printMe;
+  element.appendChild(btn);
   return element;
 }
 
